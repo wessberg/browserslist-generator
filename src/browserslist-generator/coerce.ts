009f8686a17f91fa78ea35b4b0c841b7b0c51471
wessberg <frederikwessberg@gmail.com>
2019-06-20 13:23:36 +0200

    fix(bug): fixes bug where Android version 'all' could lead to crashes

--- src/browserslist-generator/coerce.ts
+++ src/browserslist-generator/coerce.ts
@@ -8,7 +8,7 @@ import {CaniuseBrowser} from "./i-caniuse";
  * @returns {SemVer}
  */
 export function coerce(browser: CaniuseBrowser | undefined, version: string): SemVer {
-	if (browser === "op_mini" && version === "all") {
+	if ((browser === "op_mini" || browser === "android") && version === "all") {
 		return _coerce("0.0.0")!;
 	} else if (browser === "safari" && version === "TP") {
 		return _coerce("99999.0.0")!;
