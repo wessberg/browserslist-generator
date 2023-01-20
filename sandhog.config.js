17513f0f7f00f1b50ae2a487a3b752a0c54cad2a
Frederik Wessberg <frederikwessberg@gmail.com>
2023-01-20 16:43:47 +0100

    fix: fix sandhog confiog

--- sandhog.config.js
+++ sandhog.config.js
@@ -1,5 +1,7 @@
-module.exports = {
-	...require("@wessberg/ts-config/sandhog.config"),
+import baseConfig from "@wessberg/ts-config/sandhog.config.js";
+
+export default {
+	...baseConfig,
 	logo: {
 		url: "https://raw.githubusercontent.com/wessberg/browserslist-generator/master/documentation/asset/logo.png",
 		height: 150
